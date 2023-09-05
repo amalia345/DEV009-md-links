d5070c90438fa14a3b458a16bff31c7f88e2199b
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-09-04 22:36:57 -0600

    the link exiist

--- cli.js
+++ cli.js
@@ -1,25 +1,39 @@
 //import chalk  from "chalk";
-// pORQUE CHALK NO FUNCIONA EN MODULOS ES, SI PONGO 'TYPE MODULES' ENTONCES LOS 'REQUIRE' YA NO SIRVEN, PERO EN NPM CHALK DICE QUE ES AFUERZXAS IMPORT
+// pORQUE CHALK NO FUNCIONA EN MODULOS
 //import { mdLinks } from "../DEV009-md-links/index.js";
 const{ mdLinks } = require( './index.js');
+const c = require('ansi-colors');
+
 
 let path1 = '../DEV009-md-links/index.js'
 let path2 = '../DEV@09-mh-links/index.jgs'
 let path3 = '../DEV009-md-links/myFile.md'
 
 mdLinks(path3)
+
 .then((promiseResolve)=>{
-    //console.log(chalk.blue('hola desde el cli'));
-    if (promiseResolve) {
-        console.log('The file exists and its an markdown file');
+    const isMarkdown= promiseResolve[0]
+    const dataExtracted=  promiseResolve[1]
+
+    if (isMarkdown) {
+        console.log(c.bgGreen('The file exists and its an markdown file'));
+        
+        let arrayMarkdown = stringToArray(dataExtracted)
+        //crear for que identifique que elementos del array son links
+        console.log(arrayMarkdown[2]);
+
         // mandara llamar a otra FUNCION QUE SAQUE EL CONTENIDO DEL ARCHIVO READFILE Y RS PROMISES
         // EL CONTENIDO DEL ARCHIVO GUARDALO EN UN ARRAY
         // CEHCAR EL ARRAY PARA BUSCAR LINKS Y GUARDAR ESOS LINKS EN OTRO ARRAY
         // DEL NUEVO ARRAY CEHCAR CON UN CICLO SI LOS LINKS FUNCIONAN O NO
     } else {
-        console.log('The file exist but is not a markdown file');
+        console.log(c.bgMagenta('The file exist but is not a markdown file'));
     }
 })
 .catch((error)=> {
-    console.log(error);
+    console.log(c.bgRed(error));
 });
+
+function stringToArray(params) {
+    return params.split('\n')
+}
--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 
 // ----------------------imports----------------------
-const fs = require("fs"); 
+const fs = require("fs");
 const path = require('path')
 // ----------------------functions----------------------
-const mdLinks = (pathArgument, options) => {// cambiar nombrea cehcarSiEsMDextension para usarl esta funcion en en otras funciones 
+const mdLinks = (pathArgument, options) => {
   return new Promise((resolve, reject) => {
     if (fs.existsSync(pathArgument)) { // regresa el resolve la promesa si la ruta del archivo si existe
-      const extensionOfFile = path.extname(pathArgument) // la funcion extname nos regresa la extension del archivo que le demnos como argumento
-      console.log('path correcto ' + pathArgument + ' extension is :  ' + extensionOfFile);
-      let isExtensionMD; // variabel booleana vacia para decirt si el archivo es md o no
-      if (extensionOfFile === '.md' || extensionOfFile === '.markdown') {
-        isExtensionMD = true
-      } else {
-        isExtensionMD = false
+      const isExtensionMD = checkIfExtensionMD(pathArgument)
+      if (isExtensionMD) {
+        extractData(pathArgument)
+          .then(mdData => {
+            resolve([isExtensionMD, mdData])
+          })
+          .catch(err => {
+            reject("error reading the md file" +err)
+          })
       }
-      resolve(isExtensionMD)
-      // imprime la direccion actual del programa
-      // usando process argv para ver la direccion del programa 
-      //Chequear y convertir a una ruta absoluta
-
-    } else {// regresa el reject de la promesa si la ruta del archivo no existe
+      else {
+        resolve([isExtensionMD, null])
+      }
+    } else {
       reject('rejected This error means that the file doesnt exist')
     }
-    //si no existe rechazamos la promesa
+    //si no existe r`echazamos la promesa
   });
 };
+
+
+function checkIfExtensionMD(parameter) {
+  const extensionOfFile = path.extname(parameter) // la funcion extname nos regresa la extension del archivo que le demnos como argumento
+  if (extensionOfFile === '.md' || extensionOfFile === '.markdown') {
+    return true
+  } else {
+    return false
+  }
+}
+
+function extractData(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    });
+  });
+}
 // ----------------------exports----------------------
 module.exports = {
   mdLinks
